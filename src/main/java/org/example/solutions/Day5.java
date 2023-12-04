e56a3e1665156a1753673c8790ca3dc4e45dfe09
Anton Marklund <anton.marklund00@gmail.com>
2023-12-04 20:30:04 +0100

    init day 5

---
+++ src/main/java/org/example/solutions/Day5.java
@@ -0,0 +1,13 @@
+package org.example.solutions;
+
+public class Day5 implements ISolution{
+    @Override
+    public int solutionPart1() {
+        return 0;
+    }
+
+    @Override
+    public int solutionPart2() {
+        return 0;
+    }
+}
